461ec7275c86d651def7f19b300618fe075ea14c
SkyesL <51507512+SkyesL@users.noreply.github.com>
2019-06-29 23:05:33 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', message => {
+  if (!message.guild) return;
+
+  if (message.content.startsWith('+kick')) {
+    const user = message.mentions.users.first();
+    if (user) {
+      const member = message.guild.member(user);
+      if (member) {
+        member.kick('Optional reason that will display in the audit logs').then(() => {
+          message.reply(`Successfully kicked ${user.tag}`);
+        }).catch(err => {
+          message.reply('I was unable to kick the member');
+          console.error(err);
+        });
+      } else {
+        message.reply('That user isn\'t in this guild!');
+      }
+    } else {
+      message.reply('You didn\'t mention the user to kick!');
+    }
+  }
+});
+
+client.on('message', msg=>{
+  if (msg.content ===  "+help") {
+      msg.channel.send('**Lista komend:**');
+      msg.channel.send('**+kick**');
+  }
+});
+
+client.login('NTg3MzMyOTkwODMzNTI0NzQ5.XP1Ypw.98Rt1DtNhdEwTJwaiOi0Ys1xVng');
