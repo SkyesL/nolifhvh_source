3cc8ac2cc801a1e7839953c0c4218c7a1e0fda07
SkyesL <51507512+SkyesL@users.noreply.github.com>
2019-06-30 10:33:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log('I am ready!');
+  console.log('i am ready!');
 });
 
 client.on('message', message => {
@@ -15,24 +15,32 @@ client.on('message', message => {
       const member = message.guild.member(user);
       if (member) {
         member.kick('Optional reason that will display in the audit logs').then(() => {
-          message.reply(`Successfully kicked ${user.tag}`);
+          message.channel.send(`Poprawnie wyrzuciłem ${user.tag}`);
         }).catch(err => {
-          message.reply('I was unable to kick the member');
+          message.channel.send('Nie mogłem wyrzucić użytkownika');
           console.error(err);
         });
       } else {
-        message.reply('That user isn\'t in this guild!');
+        message.channel.send('Ten użytkownik nie jest w gildi');
       }
     } else {
-      message.reply('You didn\'t mention the user to kick!');
+      message.channel.send('Nie wybrałeś użytkownika do wyżucenia np. kick @tag');
     }
   }
 });
 
 client.on('message', msg=>{
   if (msg.content ===  "+help") {
-      msg.channel.send('**Lista komend:**');
+      msg.channel.send('```Lista komend:```');
       msg.channel.send('**+kick**');
+      msg.channel.send('**+pomoc**')
+
+  }
+});
+client.on('message', msg=>{
+  if (msg.content === "+pomoc") {
+      msg.channel.send('```Jeśli kick nie działa lub bot szfankuje napisz do mnie! secret-re$ident#1972, Ale gdy bot nie może wyrzucić, musi mieć permisje administratora, jeśli nie działa to urzytkownik ma wyższe permissje```')
+
   }
 });
 
